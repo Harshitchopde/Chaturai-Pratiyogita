0306992f6705fe7b74f941cb3ab8f967c2663516
Harshitchopde <chopdeharshit@gmail.com>
2025-02-05 19:09:52 +0530

    Question-view-nav-step-2-complete
    step 2 complete and prev and next show

--- src/components/core/dashboard/AddQuiz/QuestionBuilder/NestedQuestionView.jsx
+++ src/components/core/dashboard/AddQuiz/QuestionBuilder/NestedQuestionView.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { QuestionModel } from './QuestionModel';
 import ConformationPopUp from '../../../../common/ConformationPopUp';
 import { CiCirclePlus } from "react-icons/ci";
+import { MdDelete, MdEdit } from 'react-icons/md';
+import { AiFillCaretDown } from 'react-icons/ai';
 const NestedQuestionView = () => {
     const { quiz} = useSelector(state=>state.quiz);
     const { token} = useSelector(state=>state.auth);
@@ -10,10 +12,13 @@ const NestedQuestionView = () => {
     const [addQuestion,setAddQuestion] = useState(null);
     const [editQuestion,setEditQuestion] = useState(null);
     const [viewQuestion,setViewQuestion] = useState(null);
-    console.log("Quiz id ",quiz._id)
+    console.log("Quiz ",quiz)
     // conformation model
     const [conformationModel,setConformationModel] = useState(null);
      console.log("Conformation ",conformationModel);
+     const handleQuestionDelete = (questionId)=>{
+      
+     }
     return (
         <>
         <div className=" flex flex-col space-y-5">
@@ -21,6 +26,44 @@ const NestedQuestionView = () => {
            className=' border-2 flex items-center max-w-max px-3 rounded-lg py-1 border-yellow-400  text-yellow-400'>
             Question <CiCirclePlus className='ml-2 font-bold'/>
            </button>
+           <div className=" bg-white rounded-lg px-20 py-10">
+              {
+                quiz && quiz?.questions?.map((question,i)=>(
+                  <details open key={i}>
+                    <summary className=' flex   justify-between cursor-pointer items-center border-b-2 py-2'>
+                      <p>Question {i+1}. {question.questionDesc}</p>
+                        <div className="flex gap-3 items-center">
+                        <button>
+                          <MdEdit className=' text-xl'/>
+                        </button>
+                        <button
+                         onClick={()=>setConformationModel({
+                          text1:"Delete the Question",
+                          text2:"This Question Will be Deleted",
+                          btn1Text:"Delete",
+                          btn2Text:"Cancel",
+                          btn1Handler:()=>handleQuestionDelete(question._id),
+                          btn2Handler:()=>setConformationModel(null)
+                         })}>
+                          <MdDelete/>
+                        </button>
+                        <span className=' font-medium text-richblack-300'></span>
+                        <AiFillCaretDown className=' text-xl text-richblack-300' />
+                      </div>
+                    </summary>
+                    <div className="px-6 pb-4">
+                    {
+                      question?.options?.map((option,i)=>(
+                        <div className=" flex text-xl ">{i+1}. {option.text}</div>
+                      ))
+
+                    }
+                    </div>
+                  </details>
+                ))
+
+              }
+           </div>
         </div>
         {/* model  */}
         {
--- src/components/core/dashboard/AddQuiz/QuestionBuilder/index.jsx
+++ src/components/core/dashboard/AddQuiz/QuestionBuilder/index.jsx
@@ -2,16 +2,42 @@ import { data } from 'autoprefixer'
 import React, { useState } from 'react'
 import ConformationPopUp from '../../../../common/ConformationPopUp'
 import NestedQuestionView from './NestedQuestionView'
+import IconBtn from '../../../../common/IconBtn'
+import { MdNavigateNext } from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
+import toast from 'react-hot-toast'
+import { setEditQuiz, setStep } from '../../../../../slices/quizSlicer'
 
 
 const AddQuestion = () => {
-
+  const {quiz}= useSelector(state=>state.quiz);
+  const dispatch = useDispatch();
+  const goToNext = ()=>{
+    if(quiz.numberOfQuestions && quiz.questions.length !==quiz?.numberOfQuestions){
+      toast.error("Total number of questions not match : ",quiz.numberOfQuestions)
+      return;
+    }
+    dispatch(setStep(3));
+  }
+  const goToBack = ()=>{
+    dispatch(setStep(1));
+    dispatch(setEditQuiz(true));
+  }
   return (
     <>
-    <div className=' border-[1px] p-6 space-y-8 rounded-md '>
+    <div className=' border-[1px] bg-slate-100 p-6 space-y-8 rounded-md '>
       <div className=" text-2xl font-semibold">Quiz Builder</div>
-       <NestedQuestionView/>
-    
+      <NestedQuestionView/>
+      {/* prev & next button */}
+      <div className=" flex justify-end items-center gap-x-3">
+      <button onClick={goToBack}
+         className={`flex cursor-pointer items-center gap-x-2 rounded-md py-[4px] px-[20px] font-semibold bg-slate-200`}>prev</button>
+
+         <IconBtn text="Next" onClick={goToNext}>
+          {/* <MdNavigateNext/> */}
+          <MdNavigateNext/>
+         </IconBtn>
+      </div>
     </div>
    
     </>
